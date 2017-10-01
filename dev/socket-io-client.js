bae9f5d7593d003305548ae761dea817651e7ba7
Daniela Sayuri Yassuda <danielayassuda@gmail.com>
2017-10-01 18:33:55 -0300

    Working client examples

--- dev/socket-io-client.js
+++ dev/socket-io-client.js
@@ -1,11 +1,31 @@
-const socket = require('socket.io-client')('http://localhost:9090')
+const socket = require('socket.io-client')('http://ec2-54-232-254-163.sa-east-1.compute.amazonaws.com:9090')
 
 socket.on('connect', () => {
   console.log('Connected')
-  socket.emit('hello', '{"morpheusId":"mockid1234","type":"dashboard"}', ack => console.log(ack))
+  socket.emit('hello', '{"morpheusId":"adf654wae84fea5d8ea6","type":"dashboard"}', ack => console.log(ack))
 })
 
-setInterval(() => socket.emit('action', {"action":"data"}), 5000);
+const action = [
+  {
+		'topic': 'hw/000281D0',
+		'controlParameters': [
+			{
+				'parameter': 'ts',
+				'value': 150091415
+			},
+			{
+				'parameter': 'ty',
+				'value': 'rele1_action'
+			}
+	    ],
+	    'payload': {
+	    	'v1': 5,
+	    	'v2': 'auto'
+	    }
+	},
+];
+
+setInterval(() => socket.emit('actionRequest', action), 5000);
 
 socket.on('data', (data, cb) => {
   console.log(JSON.stringify(data))
--- dev/socket-io-morpheus.js
+++ dev/socket-io-morpheus.js
@@ -1,8 +1,8 @@
-const socket = require('socket.io-client')('http://localhost:9090')
+const socket = require('socket.io-client')('http://ec2-54-232-254-163.sa-east-1.compute.amazonaws.com:9090')
 
 socket.on('connect', () => {
   console.log('Connected')
-  socket.emit('hello', '{"morpheusId":"mockid1234","type":"morpheus"}', ack => console.log(ack))
+  socket.emit('hello', '{"morpheusId":"adf654wae84fea5d8ea6","type":"morpheus"}', ack => console.log(ack))
   socket.emit('confirmation', '{"x":5}');
   socket.emit('confirmationReport', '{"y":7}');
 })
